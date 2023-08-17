aa83eba73a3f01814cbd6fbf49e27d47fa97f04b
MatyBene <benedettimaty@gmail.com>
2023-08-17 00:17:41 -0300

    crea y agrega estilos a productDetail.css (version mobile)

--- src/app.js
+++ src/app.js
@@ -3,18 +3,22 @@ const path = require("path");
 const app = express();
 const PORT = 3333;
 
-app.listen(PORT,()=>console.log(`Servidor creado en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor creado en el puerto ${PORT}`));
 
-app.use(express.static(path.join(__dirname,"../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.get("/register", (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/register.html"))
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
-app.get("/productCart", (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/productCart.html"))
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/productCart.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/productDetail.html"));
 });
