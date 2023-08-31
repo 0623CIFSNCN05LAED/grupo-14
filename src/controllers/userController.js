0fd710a6a3a2047ab76d189e90b6cb9391a5c166
MatyBene <benedettimaty@gmail.com>
2023-08-30 22:43:52 -0300

    Atomiza head. Arregla enlaces del header

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const userController = {
-    login: (req,res)=>{
-        res.render("users/register");
-    },
-    register: (req,res)=>{
-        res.render("users/register");
-    }
+  login: (req, res) => {
+    res.render("users/login");
+  },
+  register: (req, res) => {
+    res.render("users/register");
+  },
 };
 
 module.exports = userController;
