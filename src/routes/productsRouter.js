0fa4ab762231bb7a618ce3cbbcb37af872f8195c
MatyBene <benedettimaty@gmail.com>
2023-09-03 14:20:11 -0300

    agrega vistas dinamicas a detalle de producto

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,8 +3,7 @@ const router = Router();
 
 const productsController = require("../controllers/productsController");
 
-router.get("/products/productDetail", productsController.detail);
+router.get("/products/productDetail/:id", productsController.detail);
 router.get("/products/productCart", productsController.cart);
-
 
 module.exports = router;
