a826fab35c92f83fd5e1f931e89f79be51042b7d
Mauro Corominas <maurocorominas@gmail.com>
2023-09-06 00:35:20 -0300

    en produccion productList

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,8 +8,9 @@ const productsController = {
     if (!product) {
       return res.render("main/homeMayorista");
     }
-
+    
     res.render("products/productDetail", { product, products });
+        
   },
   cart: (req, res) => {
     res.render("products/productCart");
@@ -17,8 +18,13 @@ const productsController = {
   create: (req, res) => {
     res.render("products/createProduct");
   },
+  
+  productsList: (req, res) =>{
+    const id = req.params.id;
+    const product = products.find((product) => product.id == id);
 
-
+    res.render("products/productList",{product,products});
+  },
 
 };
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -6,5 +6,6 @@ const productsController = require("../controllers/productsController");
 router.get("/products/productDetail/:id", productsController.detail);
 router.get("/products/productCart", productsController.cart);
 router.get("/products/createProduct", productsController.create);
+router.get("/products/productList", productsController.productsList);
 
 module.exports = router;
