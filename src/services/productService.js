41cbd9d52f677fee2ae22c6b87712fbb607d72d9
chiarabosio2023 <chiaravalentinabosio@gmail.com>
2023-09-08 00:19:42 -0300

    modifico productServices

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,8 +1,30 @@
 const products = require("../data/products");
+const formatProductPrices = function (product) {
+    const priceWithDiscount =
+      product.price - product.price * (product.discount / 100);
+  
+    product.priceWithDiscount = ` ${priceWithDiscount.toLocaleString("es", {
+      minimumFractionDigits: 2,
+    })}`;
+  
+    product.price = ` ${product.price.toLocaleString("es", {
+      minimumFractionDigits: 2,
+    })}`;
+  
+    product.discount = product.discount.toLocaleString("es");
+  
+    return product;
+  };
+  const formatProductsPrices = function (products) {
+    return products.map((product) => formatProductPrices(product));
+  };
 const productServices = {
     getInSaleProducts: ()=>{
-        return products.filter(product => product.offer==true)
+        const inSaleProducts = products.filter(product => product.offer==true);
+        console.log(inSaleProducts)
+        return formatProductsPrices(inSaleProducts);
     }
 }
+
 
 module.exports = productServices;
