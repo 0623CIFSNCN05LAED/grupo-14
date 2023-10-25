af7a7bc715fc7a4e191dc076f320e6129335037e
MatyBene <benedettimaty@gmail.com>
2023-10-24 23:47:48 -0300

    completa mensajes en validations

--- src/validations/registerValidationCf.js
+++ src/validations/registerValidationCf.js
@@ -1,33 +1,39 @@
 const { body } = require("express-validator");
 
 module.exports = [
-  body("name").notEmpty().withMessage("Debes completar el campo de nombre"),
+  body("name").notEmpty().withMessage("Debes completar el campo de nombre."),
   body("lastName")
     .notEmpty()
-    .withMessage("Debes completar el campo de nombre "),
-  body("dni").notEmpty().withMessage("Debes completar el campo de "),
-  body("tel").notEmpty().withMessage("Debes completar el campo de "),
+    .withMessage("Debes completar el campo de apellido."),
+  body("dni").notEmpty().withMessage("Debes completar el campo de DNI."),
+  body("tel")
+    .notEmpty()
+    .withMessage("Debes completar el campo de Teléfono / Celular."),
   body("email")
     .notEmpty()
-    .withMessage("Debes introducir una dirección de correo eléctronico")
+    .withMessage("Debes introducir una dirección de correo eléctronico.")
     .bail()
     .isEmail()
-    .withMessage("Debes introducir un formato de correo eléctronico válido"),
+    .withMessage("Debes introducir un formato de correo eléctronico válido."),
   body("password")
     .notEmpty()
-    .withMessage("Debes escribir una contraseña")
+    .withMessage("Debes escribir una contraseña.")
     .bail()
     .isLength({ min: 8 })
-    .withMessage("La contraseña debe tener como mínimo 8 caracteres"),
+    .withMessage("La contraseña debe tener como mínimo 8 caracteres."),
   body("confirmPassword").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error("Las contraseñas no coinciden");
+      throw new Error("Las contraseñas no coinciden.");
     }
     return true;
   }),
-  body("address").notEmpty().withMessage("Debes completar el campo de "),
-  body("location").notEmpty().withMessage("Debes seleccionar una "),
-  body("cp").notEmpty().withMessage("Debes completar el campo de "),
-  body("province").notEmpty().withMessage("Debes completar el campo de "),
-  body("country").notEmpty().withMessage("Debes completar el campo de "),
+  body("address")
+    .notEmpty()
+    .withMessage("Debes completar el campo de dirección."),
+  body("location").notEmpty().withMessage("Debes seleccionar una localidad."),
+  body("cp").notEmpty().withMessage("Debes completar el campo de C.P."),
+  body("province")
+    .notEmpty()
+    .withMessage("Debes completar el campo de provincia."),
+  body("country").notEmpty().withMessage("Debes completar el campo de país."),
 ];
--- src/validations/registerValidationM.js
+++ src/validations/registerValidationM.js
@@ -1,30 +1,38 @@
 const { body } = require("express-validator");
 
 module.exports = [
-  body("businessName").notEmpty().withMessage("Debes completar el campo de "),
-  body("cuit").notEmpty().withMessage("Debes completar el campo de "),
-  body("tel").notEmpty().withMessage("Debes completar el campo de "),
+  body("businessName")
+    .notEmpty()
+    .withMessage("Debes completar el campo de razón social."),
+  body("cuit").notEmpty().withMessage("Debes completar el campo de CUIT."),
+  body("tel")
+    .notEmpty()
+    .withMessage("Debes completar el campo de Teléfono / Celular."),
   body("email")
     .notEmpty()
-    .withMessage("Debes introducir una dirección de correo eléctronico")
+    .withMessage("Debes introducir una dirección de correo eléctronico.")
     .bail()
     .isEmail()
-    .withMessage("Debes introducir un formato de correo eléctronico válido"),
+    .withMessage("Debes introducir un formato de correo eléctronico válido."),
   body("password")
     .notEmpty()
-    .withMessage("Debes escribir una contraseña")
+    .withMessage("Debes escribir una contraseña.")
     .bail()
     .isLength({ min: 8 })
-    .withMessage("La contraseña debe tener como mínimo 8 caracteres"),
+    .withMessage("La contraseña debe tener como mínimo 8 caracteres."),
   body("confirmPassword").custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error("Las contraseñas no coinciden");
+      throw new Error("Las contraseñas no coinciden.");
     }
     return true;
   }),
-  body("address").notEmpty().withMessage("Debes completar el campo de "),
-  body("location").notEmpty().withMessage("Debes seleccionar una "),
-  body("cp").notEmpty().withMessage("Debes completar el campo de "),
-  body("province").notEmpty().withMessage("Debes completar el campo de "),
-  body("country").notEmpty().withMessage("Debes completar el campo de "),
+  body("address")
+    .notEmpty()
+    .withMessage("Debes completar el campo de dirección."),
+  body("location").notEmpty().withMessage("Debes seleccionar una localidad."),
+  body("cp").notEmpty().withMessage("Debes completar el campo de C.P."),
+  body("province")
+    .notEmpty()
+    .withMessage("Debes completar el campo de provincia."),
+  body("country").notEmpty().withMessage("Debes completar el campo de país."),
 ];
