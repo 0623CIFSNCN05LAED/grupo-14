ff91ef0e917801882b3e8b4cc59e979f363dccf9
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2023-11-02 11:28:20 -0300

    modelos products y sus relaciones. (falta agregarlos al script sql)

---
+++ src/database/models/Product.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, dataTypes)=>{
+    const alias = "Product";
+    const cols = {
+      id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: dataTypes.STRING(255),
+      shortName: dataTypes.STRING(255),
+      retailPrice: dataTypes.DECIMAL(10, 2),
+      wholesalePrice: dataTypes.DECIMAL(10, 2),
+      discount: dataTypes.INTEGER,
+      stock: dataTypes.INTEGER,
+      image: dataTypes.STRING(255),
+      category_id: dataTypes.INTEGER,
+      description: dataTypes.TEXT,
+      brand_id: dataTypes.INTEGER,
+    };
+    const config= {
+        tableName: "Products",
+        timestamps: false
+    }
+
+    const Product = sequelize.define(alias,cols,config);
+
+    Product.associate = function(models){
+        Product.belongsTo(models.Category, {
+            as: "category",
+            foreignKey: "category_id"
+        })
+
+        Product.belongsTo(models.Brand, {
+            as: "brand",
+            foreignKey: "brand_id"
+        })
+    }
+    
+
+    return Product
+
+
+}
