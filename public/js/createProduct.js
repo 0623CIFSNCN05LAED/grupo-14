562fec46ea7fcfcff8bbbe67782ead633b2dfd1a
florenciazevallos <zevallosmflorencia@gmail.com>
2023-11-23 20:19:54 -0300

    termino validaciones

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -34,6 +34,21 @@ const validations = [
     check: (input) => input.value != "",
     message: "El campo no debe estar vacío",
   },
+  {
+    field: "categoryLabel",
+    check: (input) => input.value != "",
+    message: "El campo no debe estar vacío",
+  },
+  {
+    field: "image",
+    check: (input) => input.files && input.files.length > 0,
+    message: "Cargar una imagen",
+  },
+  {
+    field: "descriptionError",
+    check: (input) => input.value != "",
+    message: "El campo no debe estar vacío",
+  },
 ];
 
 validations.forEach((validation) => {
@@ -54,22 +69,35 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const validationsResult = [];
+
+  validations.forEach((validation) => {
+    const inputId = validation.field;
+    const input = document.getElementById(inputId);
+    const inputErrorMessage = document.getElementById(inputId + "Error");
+    const result = inputValidation(validation, input, inputErrorMessage);
+    validationsResult.push(result);
+  });
+  if (validationsResult.every((val = (val) => val == true))) {
+    form.submit();
+  }
 });
 
 function imputValidation(validation, input, inputErrorMessage) {
   if (!input.value) {
     inputErrorMessage.innerText = "El campo no debe estar vacío";
     inputErrorMessage.classList.add("display");
-    return;
+    return false;
   }
 
   if (!validation.check(input)) {
     inputErrorMessage.innerText = validation.message;
     inputErrorMessage.classList.add("display");
-    return;
+    return false;
   }
 
   inputErrorMessage.innerText = "";
   inputErrorMessage.classList.remove("display");
-  return;
+  return true;
 }
