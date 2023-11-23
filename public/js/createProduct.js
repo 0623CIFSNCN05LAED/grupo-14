7733451016ad2004206147090fe67919b9e2a152
florenciazevallos <zevallosmflorencia@gmail.com>
2023-11-23 16:21:34 -0300

    agrego css de error y modifico createProduct.js agregando funci{on de validaciones

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -1,15 +1,23 @@
 const validations = [
   {
     field: "name",
-    validation: () => true,
-    message: "mensaje de validación",
+    check: (input) => input.value != "",
+    message: "El campo no debe estar vacío",
   },
 ];
 
 validations.forEach((validation) => {
   const inputId = validation.field;
-  const input = document.getElementById(imputId);
+  const input = document.getElementById(inputId);
   const inputErrorMessage = document.getElementById(inputId + "Error");
+
+  function validate() {
+    console.log("input", input.value);
+    imputValidation(validation, input, inputErrorMessage);
+  }
+
+  input.addEventListener("blur", validate);
+  input.addEventListener("input", validate);
 });
 
 const form = document.getElementById("form");
@@ -24,3 +32,15 @@ form.addEventListener("submit", (event) => {
     console.log("error");
   }
 });
+
+function imputValidation(validation, input, inputErrorMessage) {
+  if (!input.value) {
+    inputErrorMessage.innerText = "El campo no debe estar vacío";
+    inputErrorMessage.classList.add("display");
+  }
+
+  if (!validation.check(input)) {
+    inputErrorMessage.innerText = validation.message;
+    inputErrorMessage.classList.add("display");
+  }
+}
