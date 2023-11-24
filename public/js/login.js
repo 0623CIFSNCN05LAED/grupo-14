37bc564d929960301e6b360cfe722b84a661ae4a
chiarabosio2023 <chiaravalentinabosio@gmail.com>
2023-11-24 16:05:30 -0300

    agrego css del error y modifico login.js y login.ejs

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 const validations = [
     {
         field: 'email',
-        check: (input) => input.value != '',
+        check: (input) => input.value != '' && isValidEmail(input.value),
         msg: 'Debe completar con su email'
     },
     {
@@ -27,28 +27,45 @@ validations.forEach( (validation) => {
 })
 
 
-const form = document.getElementById('formLogin');
+const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('submit interceptado');
 
-    const email = document.getElementById('email');
-    const emailValue = email.value;
+    const validationsResult = [];
+    validations.forEach( (validation) => {
+        const inputId = validation.field;
+        const input = document.getElementById(inputId);
+        const inputErrorMsg = document.getElementById(inputId + 'Error');
+    
+        const result = inputValidation(validation, input, inputErrorMsg);
+        validationsResult.push(result);
+    })
 
-    if(emailValue != ''){
-
-    }else{
-        console.log('error');
+    if(validationsResult.every(val => val == true)){
+        form.submit();
     }
 });
 
 function inputValidation(validation, input, inputErrorMsg){
     if(!input.value){
         inputErrorMsg.innerText = 'El campo no debe estar vacío';
+        inputErrorMsg.classList.add('display');
+        return;
     }
 
     if(!validation.check(input)){
         inputErrorMsg.innerText = validation.msg;
+        inputErrorMsg.classList.add('display');
+        return;
     }
+
+    inputErrorMsg.innerText = '';
+    inputErrorMsg.classList.remove('display');
+    return;
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
