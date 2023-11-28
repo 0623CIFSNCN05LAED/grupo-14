f3ef82be29f63e50b8911d10358c7903f923d1c5
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2023-11-28 15:31:33 -0300

    corrije logica de crear usuario

--- src/services/userDBservice.js
+++ src/services/userDBservice.js
@@ -35,42 +35,48 @@ module.exports = {
     }
   },
   create: async function (dataUser) {
-    const newUser = {
-      id: uuidv4(),
-      email: dataUser.email,
-      password: bcrypt.hashSync(dataUser.password),
-      phoneNumber: dataUser.tel,
-      notify: dataUser.notify ? 1 : 0,
-    };
+    try{
+      const newUser = {
+        id: uuidv4(),
+        email: dataUser.email,
+        password: bcrypt.hashSync(dataUser.password),
+        phoneNumber: dataUser.tel,
+        notify: dataUser.notify ? 1 : 0,
+      };
+  
+      await User.create(newUser);
+  
+      if (dataUser.category == "cf") {
+        delete dataUser.cuit;
+        delete dataUser.businessName;
+        UserCf.create({
+          id: newUser.id,
+          name: dataUser.name,
+          lastName: dataUser.lastName,
+          dni: dataUser.dni
+        });
+      } else if (dataUser.category == "mayorista") {
+        delete dataUser.name;
+        delete dataUser.lastName;
+        delete dataUser.dni;
+        UserMayorista.create({
+          id: newUser.id,
+          cuit: dataUser.cuit,
+          businessName: dataUser.businessName,
+        });
+      } else if (dataUser.category == "admin") {
+        delete dataUser.cuit;
+        delete dataUser.businessName;
+        delete dataUser.dni;
+        UserAdmin.create({
+          id: newUser.id,
+          name: dataUser.name,
+          lastName: dataUser.lastName,
+        });
+      }
 
-    User.create(newUser);
+    }catch{
 
-    if (dataUser.category == "cf") {
-      delete dataUser.cuit;
-      delete dataUser.businessName;
-      UserCf.create({
-        id: newUser.id,
-        name: dataUser.name,
-        lastName: dataUser.lastName,
-      });
-    } else if (dataUser.category == "mayorista") {
-      delete dataUser.name;
-      delete dataUser.lastName;
-      delete dataUser.dni;
-      UserMayorista.create({
-        id: newUser.id,
-        cuit: dataUser.cuit,
-        businessName: dataUser.businessName,
-      });
-    } else if (dataUser.category == "admin") {
-      delete dataUser.cuit;
-      delete dataUser.businessName;
-      delete dataUser.dni;
-      UserAdmin.create({
-        id: newUser.id,
-        name: dataUser.name,
-        lastName: dataUser.lastName,
-      });
     }
   },
   delete: function (id) {
