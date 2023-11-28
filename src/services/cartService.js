f5ce9814704737cc0dc8ab88098b16e99ae2e8a0
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2023-11-28 14:44:45 -0300

    termina funcionalidad de crear carrito en estado ACTIVE, agregar productos y actualizar si se agregan productos.

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -20,55 +20,81 @@ module.exports = {
     console.log(error)
   }
   },
-
-  addToCart: async function(product, userId, quantity){
+  findCartProductByCartAndProduct: async function(cart, product){
     try{
-    const existingCart = await this.findCartByUserId(userId)
-    
-    let cart;
-    if (existingCart) {
-      cart = existingCart;
-    } else {
-      cart = await Cart.create({
-        id: uuidv4(),
-        user_id: userId,
-        quantity: quantity, 
-        total_price: product.priceWithDiscount * quantity, 
-        status: "active",
-        purchase_date: null
-      });
-    }
-    const cartProduct = await CartProduct.findOne({
-      where: {
-        cart_id: cart.id
-      }
-    })
-    if(cartProduct.id != product.id){
-      await CartProduct.create({
-        cart_id: cart.id,
-        product_id: product.id,
-        quantity: quantity,
-        total_price: product.priceWithDiscount * quantity, 
-        
-      })
-    } else {
-      await CartProduct.update(
-        {
-            quantity: cart.quantity + quantity,
-            total_price: product.priceWithDiscount * (cartProduct.quantity + quantity)
-        },
+      const cartProduct = await CartProduct.findOne(
         {
           where: {
             cart_id: cart.id,
             product_id: product.id
           }
         }
-      )
-    }
+      );
+      return cartProduct.dataValues
+    } catch {
 
-    }catch{
-  } 
+    }
   },
 
+  addToCart: async function(product, userId, quantity) {
+  try {
+    const existingCart = await this.findCartByUserId(userId);
+    const cartProduct = await this.findCartProductByCartAndProduct(existingCart, product);
+    let cart;
 
+
+    if (!existingCart) { /* si no existe carrito */
+      cart = await Cart.create({
+        id: uuidv4(),
+        user_id: userId,
+        quantity: quantity,
+        total_price: product.priceWithDiscount * quantity,
+        status: "active",
+        purchase_date: null,
+      });
+
+
+      await CartProduct.create({
+        cart_id: cart.id,
+        product_id: product.id,
+        quantity: quantity,
+        total_price: product.priceWithDiscount * quantity,
+      });
+    } else { /* si existe carrito */
+      cart = existingCart;
+
+
+      if (cartProduct && cartProduct.product_id == product.id) { /* si existe carrito y producto */
+        await CartProduct.update(
+          {
+            quantity: Number(cartProduct.quantity) + Number(quantity),
+            total_price: product.priceWithDiscount * (Number(cartProduct.quantity) + Number(quantity)),
+          },
+          {
+            where: { product_id: product.id, cart_id: cart.id },
+          }
+        );
+      } else { /* si existe carrito pero NO producto */
+        await CartProduct.create({
+          cart_id: cart.id,
+          product_id: product.id,
+          quantity: quantity,
+          total_price: product.priceWithDiscount * quantity,
+        });
+      }
+
+      await Cart.update(
+        {
+          quantity: await CartProduct.sum("quantity", {where: {cart_id: cart.id}}), 
+          total_price: await CartProduct.sum("total_price", {where: {cart_id: cart.id}}),
+        },
+        {
+          where: { user_id: userId },
+        }
+      );
+    }
+  } catch (error) {
+    console.error('Error al agregar al carrito:', error);
+  }
+}
 }
