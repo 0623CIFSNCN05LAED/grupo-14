8b1ba4b47653ee47af73e0b4ec691df6293d8df4
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2023-11-29 21:31:09 -0300

    agrega middleware a ruta viewCart

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -5,8 +5,12 @@ const router = Router();
 /*************** Controller require ******************/
 const cartController = require("../controllers/cartController");
 
+/*************** Middlewares require ******************/
+const authMiddleware = require("../middlewares/authMiddleware");
+
+
 /*************** Product cart ******************/
-router.get("/", cartController.viewCart);
+router.get("/",authMiddleware, cartController.viewCart);
 
 /* ************* Add to cart ****************** */
 router.post("/addToCart", cartController.addToCart)
