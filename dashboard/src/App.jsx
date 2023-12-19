7f02ec2ed5e75748b2af0a916809513ef7a53102
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2023-12-19 13:32:29 -0300

    spa primer armado no guarde archivo

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Dashboard from './components/Dashboard'
+import {Link, Switch, Route} from "react-router-dom"
 
 
 function App() {
 
   return (
     <>
+      <Link to="/dashboard" exact="true">Dashboard</Link>
+      <Switch>
+      <Route path="/dashboard"  />
 
+      </Switch>
 
       <Dashboard />
     </>
