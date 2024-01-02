618574f1f6bd563f8fec747c275b6eb2c4aaeab0
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-01-02 12:22:48 -0300

    corrije implementacion de middlewares autorizacion usuarios m y cf

--- cleanWave/src/routes/productsCfRouter.js
+++ cleanWave/src/routes/productsCfRouter.js
@@ -2,17 +2,14 @@
 const { Router } = require("express");
 const router = Router();
 
-/*************** Middlewares require ******************/
-const userMCanNotPass = require("../middlewares/userMMiddleware");
-
 /*************** Controller require ******************/
 const productController = require("../controllers/productController");
 
 /*************** Get all products ******************/
-router.get("/", userMCanNotPass, productController.listCf);
+router.get("/", productController.listCf);
 
 /*************** Get one product ******************/
-router.get("/detail/:id", userMCanNotPass, productController.detailCf);
+router.get("/detail/:id", productController.detailCf);
 
 /* ************ Search product ******************** */
 router.get("/searchProductsCf", productController.searchProductsCf);
