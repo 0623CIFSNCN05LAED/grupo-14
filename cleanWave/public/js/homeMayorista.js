88365c9570c0c8ac3237f27387fe017184bf273a
Mauro Corominas <maurocorominas@gmail.com>
2024-01-03 13:30:04 -0300

    se arreglo menu burgerKing

--- cleanWave/public/js/homeMayorista.js
+++ cleanWave/public/js/homeMayorista.js
@@ -1,31 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function restartAnimation() {
+    const animatedItems = document.querySelectorAll('.benefitsList .animated-list-item');
 
-  /* EFECTO LETRAS */
-  document.addEventListener('DOMContentLoaded', function () {
-    function restartAnimation() {
-      const animatedItems = document.querySelectorAll('.benefitsList .animated-list-item');
-  
-      animatedItems.forEach(function (item) {
-        item.classList.remove('initial-hidden');
-        item.style.animation = 'none';
-        // Force a reflow to cancel the animation
-        void item.offsetWidth;
-        item.style.animation = null;
-        item.classList.add('initial-hidden');
-      });
-    }
-  
-    // Inicia la animación al cargar la página
-    restartAnimation();
-  
-    // Configura la repetición cada 30 segundos
-    setInterval(restartAnimation, 15000);
-  
     animatedItems.forEach(function (item) {
-      item.addEventListener('animationstart', function () {
-        item.classList.remove('initial-hidden');
-      });
+      item.classList.remove('initial-hidden');
+      item.style.animation = 'none';
+      // Force a reflow to cancel the animation
+      void item.offsetWidth;
+      item.style.animation = null;
+      item.classList.add('initial-hidden');
+    });
+  }
+
+  // Configura la repetición cada 30 segundos
+  setInterval(restartAnimation, 15000);
+
+  restartAnimation(); // Llamada inicial para la carga de la página
+
+  // Ahora agregamos el evento a cada elemento dentro de la función restartAnimation
+  document.querySelectorAll('.benefitsList .animated-list-item').forEach(function (item) {
+    item.addEventListener('animationstart', function () {
+      item.classList.remove('initial-hidden');
     });
   });
-  
-
-  
+});
